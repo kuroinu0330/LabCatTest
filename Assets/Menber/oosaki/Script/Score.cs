c7f4f340005d5c69ca24861921f7494e0d6736bf
Kuribayashi_Hiroto <20c1010002kh@edu.tech.ac.jp>
2023-10-13 11:44:00 +0900

    oosaki
    cat

--- Assets/Menber/oosaki/Script/Score.cs
+++ Assets/Menber/oosaki/Script/Score.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Score : MonoBehaviour
+public class Timer : MonoBehaviour
 {
     public float timeLimit = 60.0f; // タイムリミット（秒）
     private float timer; // タイマーのカウントダウン時間
     public Text timerText; // タイマー表示用のTextコンポーネント
+    private int catCount; // 猫の数
+    public Text catCountText; // 猫の数を表示するTextコンポーネント
 
     // Start is called before the first frame update
     void Start()
     {
         timer = timeLimit; // タイマーを初期化
+        catCount = GameObject.FindGameObjectsWithTag("Cat").Length; // 猫の数を取得
+        UpdateCatCountDisplay(); // 猫の数を表示
     }
 
     // Update is called once per frame
@@ -33,6 +37,11 @@ public class Score : MonoBehaviour
     void UpdateTimerDisplay()
     {
         timerText.text = "Time: " + timer.ToString("F1"); // タイマーの残り時間を表示
+    }
+
+    void UpdateCatCountDisplay()
+    {
+        catCountText.text = "Cats: " + catCount; // 猫の数を表示
     }
 
     void TimerExpired()
